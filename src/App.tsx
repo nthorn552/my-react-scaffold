d659e2a925f9115b9997869e0d0709efb14b3b83
surely <nthorn552@gmail.com>
2022-06-13 17:54:18 -0400

    use new devtool for redux lib

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 import rootReducer from "./reducers";
 
